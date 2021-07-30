0799aa0b850945cfc5d46a61edb77cb2e2449216
Henry Majewski <henryjmajewski@icloud.com>
2021-07-30 10:25:24 -0400

    Fixed data persistence

--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 class AssignmentList: ObservableObject {
-    @Published var items = [AssignmentItem(course: "Math", description: "Do algebra", dueDate: Date()),
-                            AssignmentItem(course: "Science", description: "Do chemistry", dueDate: Date()),
-                            AssignmentItem(course: "ELA", description: "Read", dueDate: Date())]
+    @Published var items : [AssignmentItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "data")
+            }
+        }
+    }
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "data") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([AssignmentItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        items = []
+    }
+    
 }
